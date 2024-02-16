de0167fd1319e08b8a7c71b92e21ca17b7228a22
binsarjr <binsarjr121@gmail.com>
2024-02-17 00:10:47 +0700

    remove prev year

--- services/service.js
+++ services/service.js
@@ -1,18 +1,26 @@
-const {
-    liburnasional
-} = require("./liburnasional")
-const {
-    tanggalan
-} = require("./tanggalan")
+const { liburnasional } = require("./liburnasional");
+const { tanggalan } = require("./tanggalan");
 
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
-const datapath = path.join(__dirname, '../data')
-if (!fs.existsSync(datapath)) fs.mkdirSync(datapath, {
-    recursive: true
-})
+const datapath = path.join(__dirname, "../data");
+if (!fs.existsSync(datapath))
+	fs.mkdirSync(datapath, {
+		recursive: true,
+	});
 
+fs.readdir(datapath, (err, files) => {
+	const current_year = new Date().getFullYear();
+	files.map((file) => {
+		const year = +file.split(".")[0];
+		if (year < current_year) {
+			fs.unlink(path.join(datapath, file), (err) => {
+				if (err) throw err;
+			});
+		}
+	});
+});
 
 /**
  * Fungsi utama untuk menulis daftar hari libur ke file.
@@ -21,11 +29,10 @@ if (!fs.existsSync(datapath)) fs.mkdirSync(datapath, {
  * @returns Tidak ada.
  */
 const main = async (tahun) => {
-    tahun = tahun || new Date().getFullYear()
-    const daftarHariLibur = await Promise.any([tanggalan(tahun)])
-    const outputfile = path.join(datapath, tahun + '.json')
-    fs.writeFileSync(outputfile, JSON.stringify(daftarHariLibur, null, 2))
-}
+	tahun = tahun || new Date().getFullYear();
+	const daftarHariLibur = await Promise.any([tanggalan(tahun)]);
+	const outputfile = path.join(datapath, tahun + ".json");
+	fs.writeFileSync(outputfile, JSON.stringify(daftarHariLibur, null, 2));
+};
 
-
-main()
+main();
